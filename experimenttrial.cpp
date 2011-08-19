21a9504bf119576be156e131e9ee39225df3f077
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-19 15:32:12 +0300

    Adding second object and initial calculations for that object's location

--- experimenttrial.cpp
+++ experimenttrial.cpp
@@ -1,5 +1,7 @@
 #include "experimenttrial.h"
 
+#include <QtCore/qmath.h>
+
 ExperimentTrial::ExperimentTrial(int id, RotationDirection direction, int distance, int size, int angle, QPoint gridPosition)
 {
     this->id = id;
@@ -12,14 +14,30 @@ ExperimentTrial::ExperimentTrial(int id, RotationDirection direction, int distan
     this->gridPosition = gridPosition;
 
     this->createUI();
+
+    this->setFixed(true);
 }
 
 void ExperimentTrial::createUI()
 {
+    int x = 150;
+    int y = 150;
+
     MultiWidgets::Widget * a = new MultiWidgets::Widget();
     a->setWidth(this->size);
     a->setHeight(this->size);
     a->setColor(100,0,0,50);
 
+    a->setCenterLocation( Nimble::Vector2(x, y) );
+
     this->addChild(a);
+
+    MultiWidgets::Widget * b = new MultiWidgets::Widget();
+    b->setWidth(this->size);
+    b->setHeight(this->size);
+    b->setColor(0,0,100,50);
+
+    b->setCenterLocation( Nimble::Vector2( qSin( this->angle ) * this->distance * x , qCos( this->angle * this->distance * y) ));
+
+    this->addChild(b);
 }
