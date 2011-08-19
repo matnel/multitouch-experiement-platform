4654011c0ecc2bee18ddb76ed222f8a9c9796988
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-19 13:34:12 +0300

    Created the constructor

--- experimenttrial.cpp
+++ experimenttrial.cpp
@@ -3,4 +3,11 @@
 ExperimentTrial::ExperimentTrial(int id, RotationDirection direction, int distance, int size, int angle, QPoint gridPosition)
 {
     this->id = id;
+    this->direction = direction;
+
+    this->distance = distance;
+    this->size = size;
+    this->angle = angle;
+
+    this->gridPosition = gridPosition;
 }
