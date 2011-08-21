539e29adf250e380868b892f32b6f7f7c3b02d8e
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-21 13:21:39 +0300

    Widget shall not have rotation or velocity when the interaction ends

--- locationawarewidget.cpp
+++ locationawarewidget.cpp
@@ -4,7 +4,8 @@ LocationAwareWidget::LocationAwareWidget()
 {
 }
 
-void LocationAwareWidget::interactionEnd()
+void LocationAwareWidget::interactionEnd(MultiWidgets::GrabManager &input)
 {
-
+    this->setRotation(0);
+    this->setVelocity(0,0);
 }
