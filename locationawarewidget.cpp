4ddd118555ecf5bd48359c881887f10076d65be7
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-21 13:57:30 +0300

    LocationAwareWidgets have a target location

--- locationawarewidget.cpp
+++ locationawarewidget.cpp
@@ -1,5 +1,7 @@
 #include "locationawarewidget.h"
 
+#include <QDebug>
+
 LocationAwareWidget::LocationAwareWidget()
 {
 }
@@ -9,4 +11,16 @@ void LocationAwareWidget::interactionEnd(MultiWidgets::GrabManager &input)
     MultiWidgets::Widget::interactionEnd(input);
     this->setRotation(0);
     this->setVelocity(0,0);
+}
+
+void LocationAwareWidget::input(MultiWidgets::GrabManager &gm, float dt)
+{
+    MultiWidgets::Widget::input(gm, dt);
+    qDebug() << this->location().x;
+}
+
+void LocationAwareWidget::setTarget(int x, int y)
+{
+    this->targetX = x;
+    this->targetY = y;
 }
--- locationawarewidget.h
+++ locationawarewidget.h
@@ -8,6 +8,11 @@ class LocationAwareWidget : public MultiWidgets::Widget
 public:
     LocationAwareWidget();
     virtual void interactionEnd (MultiWidgets::GrabManager &input);
+    virtual void input(MultiWidgets::GrabManager &gm, float dt);
+    void setTarget(int x, int y);
+private:
+    int targetX;
+    int targetY;
 };
 
 #endif // LOCATIONAWAREWIDGET_H
