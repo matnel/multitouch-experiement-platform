9e8921c27fc05d4b71e42b70a92a3577b02d0304
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-21 17:32:19 +0300

    Abstract handling of trials into a separate class

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,40 @@
 #include "mainwindow.h"
 
+#include <QFile>
+#include "datareader.h"
+
 MainWindow::MainWindow()
 {
+    this->currentTrial = 0;
+
+    QFile * f = new QFile("example.xml");
+    DataReader * data = new DataReader( f );
+
+    this->trials = data->trials();
+
+    this->setFixed(true);
+    this->setAllowRotation(false);
+
+    this->setInputTransparent(true);
+    this->setColor(0, 0, 0, 0);
+
+    // initial setup
+    ExperimentTrial * trial;
+    trial = trials[ 0 ];
+    this->addChild(trial);
+
+}
+
+void MainWindow::nextTrial()
+{
+    ExperimentTrial * trial;
+    trial = trials[ this->currentTrial ];
+    trial->hide();
+    this->currentTrial++;
+    if( this->currentTrial > this->trials.size() ) {
+        return;
+    }
+    trial = trials[ this->currentTrial ];
+    trial->show();
+    this->addChild(trial);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,16 @@
 #define MAINWINDOW_H
 
 #include <MultiWidgets/Widget.hpp>
+#include "experimenttrial.h"
 
 class MainWindow : public MultiWidgets::Widget
 {
 public:
     MainWindow();
+private:
+    int currentTrial;
+    void nextTrial();
+    std::vector<ExperimentTrial *> trials;
 };
 
 #endif // MAINWINDOW_H
