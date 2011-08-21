4b88f0e80bbb2759025a862bb4428f8c745a3e28
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-21 17:14:53 +0300

    Main application logic

---
+++ mainwindow.h
@@ -0,0 +1,12 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <MultiWidgets/Widget.hpp>
+
+class MainWindow : public MultiWidgets::Widget
+{
+public:
+    MainWindow();
+};
+
+#endif // MAINWINDOW_H
