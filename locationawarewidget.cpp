6390dc1bf6386bac7b1056562101108447808e26
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-21 13:25:04 +0300

    call super method too

--- locationawarewidget.cpp
+++ locationawarewidget.cpp
@@ -6,6 +6,7 @@ LocationAwareWidget::LocationAwareWidget()
 
 void LocationAwareWidget::interactionEnd(MultiWidgets::GrabManager &input)
 {
+    MultiWidgets::Widget::interactionEnd(input);
     this->setRotation(0);
     this->setVelocity(0,0);
 }
