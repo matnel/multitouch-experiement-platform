70eb1c4d40777f62e5cad18bedbb194b4a5edf06
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-09-06 17:52:14 +0300

    Removed debug, refactored code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,12 +18,10 @@ MainWindow::MainWindow(MultiWidgets::GrabManager * application)
     this->setColor(0, 0, 0, 0);
 
     this->status = new MultiWidgets::TextBox();
-    this->status->setText("Hello World!");
+    this->status->setText("");
     this->addChild( this->status );
     this->status->setLocation(800,0);
 
-    qDebug() << "MainWindow";
-    qDebug() << application;
     this->application = application;
 
     // initial setup
@@ -33,24 +31,29 @@ MainWindow::MainWindow(MultiWidgets::GrabManager * application)
 
 void MainWindow::nextTrial()
 {
-    ExperimentTrial * trial;
-    qDebug() << this->currentTrial;
+    ExperimentTrial * trial;;
     trial = trials[ this->currentTrial ];
     // causes seg fault!
     // this->deleteChild(trial);
+
     this->currentTrial++;
+
+    // when finished, show finished text
     if( this->currentTrial >= this->trials.size() ) {
         MultiWidgets::TextBox * text = new MultiWidgets::TextBox();
         text->setText("Test is over, thank you!");
         this->addChild( text );
         return;
     }
+
     trial = trials[ this->currentTrial ];
     trial->show();
     trial->setApplication( this->application );
     trial->eventAddListener("next_trial", "next_trial", this );
+
     QString s = QString::number( this->currentTrial + 1) + " of " + QString::number( this->trials.size() );
     this->status->setText( s.toStdString() );
+
     this->addChild(trial);
 }
 
