71753b18a5fb9be35c7b8d4d31fc74c908e54751
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-09-06 17:03:01 +0300

    Round elements

--- roundwidget.cpp
+++ roundwidget.cpp
@@ -1,5 +1,20 @@
 #include "roundwidget.h"
 
-RoundWidget::RoundWidget()
+#include "Luminous/Utils.hpp"
+
+RoundWidget::RoundWidget() : MultiWidgets::Widget()
 {
+}
+
+void RoundWidget::renderContent(Luminous::RenderContext &render)
+{
+    Luminous::Utils::glUsualBlend();
+
+    int r = this->width() / 2;
+
+    // TODO: Should use the set colors!
+    glColor4f( 0.2, 0.5, 0.1, 1 );
+
+    Luminous::Utils::glFilledCirclef( this->location().x +  r, this->location().y + r, r, 1000000 );
+
 }
--- roundwidget.h
+++ roundwidget.h
@@ -1,10 +1,13 @@
 #ifndef ROUNDWIDGET_H
 #define ROUNDWIDGET_H
 
-class RoundWidget
+#include "MultiWidgets/Widget.hpp"
+
+class RoundWidget : public MultiWidgets::Widget
 {
 public:
     RoundWidget();
+    virtual void renderContent(Luminous::RenderContext & r);
 };
 
 #endif // ROUNDWIDGET_H
