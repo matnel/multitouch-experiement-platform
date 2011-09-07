13e5b51e9907ea2e6efea94c1ec99ecdfa0fbd2d
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-09-07 15:50:38 +0300

    Allow setting color of widget

--- roundwidget.cpp
+++ roundwidget.cpp
@@ -4,6 +4,10 @@
 
 RoundWidget::RoundWidget() : MultiWidgets::Widget()
 {
+    this->red = 0.5;
+    this->green = 0.2;
+    this->blue = 0;
+    this->alpha = 1;
 }
 
 void RoundWidget::renderContent(Luminous::RenderContext &render)
@@ -13,8 +17,16 @@ void RoundWidget::renderContent(Luminous::RenderContext &render)
     int r = this->width() / 2;
 
     // TODO: Should use the set colors!
-    glColor4f( 0.2, 0.5, 0.1, 1 );
+    glColor4f( red, green, blue, alpha );
 
     Luminous::Utils::glFilledCirclef( this->location().x +  r, this->location().y + r, r, 1000000 );
 
+}
+
+void RoundWidget::setColor(float r, float g, float b, float a)
+{
+    this->red = r;
+    this->green = g;
+    this->blue = b;
+    this->alpha = a;
 }
--- roundwidget.h
+++ roundwidget.h
@@ -8,6 +8,12 @@ class RoundWidget : public MultiWidgets::Widget
 public:
     RoundWidget();
     virtual void renderContent(Luminous::RenderContext & r);
+    virtual void setColor(float r, float g, float b, float a);
+private:
+    float red;
+    float blue;
+    float green;
+    float alpha;
 };
 
 #endif // ROUNDWIDGET_H
